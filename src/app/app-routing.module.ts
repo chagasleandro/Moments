9fd1a1803fb9c9f21f6cfa31b41369127de2b5d9
chagasleandro <leandro_srs20@yahoo.com.br>
2023-01-07 21:39:42 -0300

    create folder new-moment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +7,7 @@ import { HomeComponent } from './components/pages/home/home.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'moments/new', component: NewMomentComponent },
 ];
 
 @NgModule({
